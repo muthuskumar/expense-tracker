043e7d182328c6a03a270380801f3c91639b7038
muthuskumar <muthuksubbiah@gmail.com>
2018-01-27 00:04:23 +0530

    Configured gulp file for server.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,33 @@
 // Gulp task runner for whole project.
 var gulp = require('gulp');
 var eslint = require('gulp-eslint');
+var nodemon = require('gulp-nodemon');
+var colors = require('ansi-colors');
 
-gulp.task('lint', () => {
+function onServerLog(log) {
+    console.log(colors.white('[') +
+        colors.yellow('nodemon') +
+        colors.white('] ') +
+        log.message);
+}
+
+gulp.task('lint:server', () => {
     return gulp.src(['**/*.js', '!node_modules/**', '!angular-src/**'])
 	.pipe(eslint())
 	.pipe(eslint.format())
 	.pipe(eslint.failAfterError());
+});
+
+gulp.task('start:server', () => {
+    let stream = nodemon({
+	script: 'server/app.js'
+    });
+
+    stream
+	.on('restart', () => {
+	    onServerLog("Server restarted");	    
+	})
+	.on('log', onServerLog);
 });
 
 gulp.task('default', () => {
