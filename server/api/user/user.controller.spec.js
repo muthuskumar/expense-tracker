3bd5f20e27cefe49047ef2def219d880a3ee3971
muthuskumar <muthuksubbiah@gmail.com>
2018-03-04 22:49:36 +0530

    Initial controller test cases created.

---
+++ server/api/user/user.controller.spec.js
@@ -0,0 +1,13 @@
+describe('User Controller ', function() {
+    it('should get all users');
+    it('should get all active users');
+    it('should find a particular user by user id');
+    it('should find a particular user by username');
+    it('should find a particular user by email');
+    it('should create a new user with provided details');
+    it('should update a particular user details');
+    it('should remove a particular user');
+    it('should authenticate an user');
+    it('should return error message & status if an user is not found');
+    it('should return error message & status if an error occurs in database operation');
+});
