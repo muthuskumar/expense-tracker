78acea25366925ce9aa15a7105116f1a2be658b5
muthuskumar <muthuksubbiah@gmail.com>
2018-06-07 10:48:35 +0530

    Code format

--- server/api/auth/output-serializers/token.serializer.js
+++ server/api/auth/output-serializers/token.serializer.js
@@ -7,17 +7,11 @@ var baseController = new BaseController();
 
 export default function tokenSerializer(user, req, res) {
     logger.info('---------------AuthController.reqLoginCb---------------');
-    
+
     const jwtTokenAuth = new JWTTokenAuth();
     const tokenResult = jwtTokenAuth.signUserId(user._id);
 
     tokenResult
-    .then(baseController.respondWithResult(res, 201))
-    .catch(baseController.handleErrorAsync(res));
-        /* .then((token) => {
-            return res.status(201).json({ token: token });
-        })
-        .catch((err) => {
-            return res.status(400).json({ errors: { name: err.name, message: err.message } });
-        }); */
+        .then(baseController.respondWithResult(res, 201))
+        .catch(baseController.handleErrorAsync(res));
 };
